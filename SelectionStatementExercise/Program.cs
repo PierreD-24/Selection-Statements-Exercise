6ce7995f999b8d86308fa35dd4ee4ae9272c89f9
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-20 11:58:58 -0700

    added a loop that allows multiple guesses

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,21 +6,25 @@
         {
             int favNumber = 24;
 
-            Console.WriteLine("Try to guess my favorite number");
-            var userInput = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Try to guess my favorite number between 1-100!");
+                var userInput = int.Parse(Console.ReadLine());
 
-            if (userInput < favNumber)
-            {
-                Console.WriteLine("Too low");
-            }
-            else if (userInput > favNumber)
-            {
-                Console.WriteLine("Too high");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+            } while (true);
         }
     }
 }
